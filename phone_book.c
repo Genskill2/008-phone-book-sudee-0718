494fc42b47e0986c551051ffc2643983e6fa0f4f
sudee-0718 <64221384+sudee-0718@users.noreply.github.com>
2021-07-08 11:36:13 +0530

    modified

--- phone_book.c
+++ phone_book.c
@@ -62,7 +62,20 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     exit(0);
   } else if (strcmp(argv[1], "search") == 0) {  /* Handle search */
-    printf("NOT IMPLEMENTED!\n"); /* TBD  */
+    //printf("NOT IMPLEMENTED!\n"); /* TBD  */
+    if (argc != 3) {
+      print_usage("Improper arguments for search", argv[0]);
+      exit(1);
+    }
+    FILE *fp = open_db_file();
+    char *name = argv[2];
+    if (!search(fp, name)) {
+      printf("no match\n");
+      fclose(fp);
+      exit(1);
+    }
+    fclose(fp);
+    exit(0);
   } else if (strcmp(argv[1], "delete") == 0) {  /* Handle delete */
     if (argc != 3) {
       print_usage("Improper arguments for delete", argv[0]);
@@ -94,7 +107,11 @@ FILE *open_db_file() {
   
 void free_entries(entry *p) {
   /* TBD */
-  printf("Memory is not being freed. This needs to be fixed!\n");  
+ // printf("Memory is not being freed. This needs to be fixed!\n");  
+  while(p!=NULL)
+  {
+    free(ret);
+  }
 }
 
 void print_usage(char *message, char *progname) {
@@ -178,15 +195,36 @@ void add(char *name, char *phone) {
 void list(FILE *db_file) {
   entry *p = load_entries(db_file);
   entry *base = p;
+  int count = 0;
   while (p!=NULL) {
     printf("%-20s : %10s\n", p->name, p->phone);
     p=p->next;
+    count++;
   }
   /* TBD print total count */
+  printf("Total entries : %d\n",count);
   free_entries(base);
 }
 
-
+int search(FILE *db_file,char *name)
+{
+    entry *p = load_entries(db_file);
+    entry *base = p;
+    int found = 0;
+    //while (p!=NULL){
+    if(strcmp(p->name,name) == 0){
+            //Implement search
+       printf("%s\n",p->phone);
+        }
+        
+    else{
+    return found;
+    }
+    //}
+    write_all_entries(base);
+    free_entries(base);
+    
+}
 int delete(FILE *db_file, char *name) {
   entry *p = load_entries(db_file);
   entry *base = p;
@@ -207,7 +245,25 @@ int delete(FILE *db_file, char *name) {
       */
 
       /* TBD */
+      if (strcmp(p->name,head->name) == 0)
+      {
+        head =prev->next;
+        free(prev);
+      }
+     del = p;
+     prev  = prev->next;
+     del  = prev->next;
+     prev->next = del->next;
+     free(del); //delete memory of node tobe deleted
+     
+     //to delete first(head) node special case
+     
     }
+    else 
+    {
+        return deleted;
+    }
+    
   }
   write_all_entries(base);
   free_entries(base);
